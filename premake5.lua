8dbb8bbc02a3c7f9f7059661d771c4f6b1287ec5
rogansteeven <rogansteeven@gmail.com>
2022-07-28 11:43:14 +0700

    Clean up premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
     kind "StaticLib"
     language "C++"
+	cppdialect "C++17"
+	staticruntime "On"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -22,8 +24,6 @@ project "ImGui"
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
 
         defines
         {
@@ -33,8 +33,6 @@ project "ImGui"
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
 
 	filter "configurations:Debug"
 		runtime "Debug"
